315e1b8c7c494ee6fde3bcfa0f2cb0d73d96de36
SqRoots <lixuan159@gmail.com>
2017-09-23 21:44:05 +0800

    js ok

--- src/js/load-contents.js
+++ src/js/load-contents.js
@@ -6,45 +6,25 @@ var args_tree2 = args.tree2 || "";
 axios.get('./contents-data.json')
   .then(function(response) {
     var tree_data = response.data;
-    //注册组件
-    //目录
-    Vue.component('tree-folder', {
-      props: ['folder'],
-      template: '<div>' +
-                  '<span :data=folder.key>{{ folder.name }}</span>' +
-                  '<tree-folder-contents :children="folder.children"/>' +
-                '</div>'
-    });
-    //目录内容
-    Vue.component('tree-folder-contents', {
-      props: ['children'],
-      template: '<ul>' +
-                  '<li v-for="child in children">' +
-                    '<tree-folder v-if="child.children" :folder="child"/>' +
-                    '<span v-else v-on:click="loadPage(child)" :data=child.key :class="{ active: child.isActive }">{{ child.name }}</span>' +
-                  '</li>' +
-                '</ul>',
-      methods: {
-        //点击导航 加载页面
-        loadPage: function(child) {
-          var tree1=event.path[3].firstChild.attributes.data.value,
-              tree2=child.key;
-          if(tree1==="management"){
-            location.href="admin/index.php";
-            return "";
-          }
-          nav.children=setActiveFalse(nav.children);  //设置导航所有项目的 isActive 为 false
-          child.isActive=true;                        //设置导航当前项目的 isActive 为 true
-          document.getElementById('article').innerHTML=tree1+'--'+tree2;
-          window.history.pushState({},0,"?tree1="+tree1+"&tree2="+tree2);
-        }
-      }
-    });
+    for(i=0;i<tree_data.length;i++){
+      tree_data[i].isActive=false;
+    }
+    console.log(tree_data);
     // 创建根实例
     var nav=new Vue({
       el: '#nav',
       data: {
-        children: tree_data
+        contents_data: tree_data
+      },
+      methods: {
+        //加载页面
+        loadPage: function(item){
+          for(i=0;i<nav.contents_data.length;i++){
+            nav.contents_data[i].isActive=false;
+          }
+          item.isActive=true;
+          console.log(item);
+        }
       }
     });
   })
